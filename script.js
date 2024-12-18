1a9f0367f25ee0bf8f2b73b62f3d164a23784c46
DylanMZ <dylanmazurzgainer@gmail.com>
2024-12-18 14:12:28 -0300

    GENERAL CHANGES AND ADDS

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    if (navMenu.classList.contains('active')) {
+        // Si está activo, añade la clase `closing` y elimina `active` después
+        navMenu.classList.add('closing');
+        navMenu.classList.remove('active');
+        hamburger.classList.remove('open');
+
+        // Después de la animación, oculta completamente el menú
+        setTimeout(() => {
+            navMenu.classList.remove('closing');
+        }, 300); // Coincide con la duración de la animación de salida
+    } else {
+        // Si no está activo, muéstralo con la animación de entrada
+        navMenu.classList.add('active');
+        hamburger.classList.add('open');
+    }
+});
+
+
 window.addEventListener('scroll', () => {
     const header = document.getElementById('header');
 
